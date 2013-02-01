b230e26da1adec68df26e2b8d7f20b3ec250e283
Jonathan Thorpe <johnny.frenchy@gmail.com>
2013-02-01 12:06:32 +0100

    [rails] Created application.

---
+++ VSCWebsiteApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VSCWebsiteApp::Application.config.secret_token = 'a9241f41bfb3af7dfd8ae19a31ec9002f5ef2251fd394e94030eae35e04d6cf87ca399d0c6f5f7444669374c83178002d089624ca392c5383709b91b26c456c9'
---
+++ VSCWebsiteApp/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+VSCWebsiteApp::Application.config.session_store :cookie_store, key: '_VSCWebsiteApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# VSCWebsiteApp::Application.config.session_store :active_record_store
