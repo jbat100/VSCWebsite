0b56a50e4cf37eb4572a26424f667487a39eff32
Jonathan Thorpe <johnny.frenchy@gmail.com>
2013-02-08 18:58:51 +0100

    [rails] Playing with TDD.

--- VSCWebsiteApp/spec/requests/static_pages_spec.rb
+++ VSCWebsiteApp/spec/requests/static_pages_spec.rb
@@ -1,20 +1,47 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+  
   describe "Home Page" do
+
     it "should have the content 'Sample App'" do
-      visit 'static_pages/home'
-      page.should_have_content('Sample App')
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
     end
+
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => "Sample App")
+    end
+
+
+    it "should have the tile 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+
   end
+
+  describe "About page" do
+    
+    it "should have the content 'about us'" do
+      visit '/static_pages/about'
+      page.should have_content('About us') 
+    end
+
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => "About Us")
+    end
+
+    it "should have the tile 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+
+
+  end 
+
 end
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
-  end
-end
+
