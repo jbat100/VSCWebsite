e7969a62f2b8aa38f3dae8fa19b31bbbef617da0
Jonathan Thorpe <johnny.frenchy@gmail.com>
2013-03-04 18:27:49 +0100

    [rails] around the end of chapter 6.

--- VSCWebsiteApp/db/schema.rb
+++ VSCWebsiteApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130201114929) do
+ActiveRecord::Schema.define(:version => 20130304170114) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -26,5 +26,7 @@ ActiveRecord::Schema.define(:version => 20130201114929) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
